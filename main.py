c74a820a67b58dfa8e39d6509f257f3d7e930325
dzes999 <101880201+dzes999@users.noreply.github.com>
2023-03-25 01:36:44 +0100

    bug fix

--- main.py
+++ main.py
@@ -3,24 +3,59 @@ from tkinter import filedialog
 import pandas as pd
 
 root = tk.Tk()
-root.withdraw()
+root.title("CSV Merger")
 
-file_paths = filedialog.askopenfilenames(title="Select CSV files to merge",
-                                         filetypes=[("CSV Files", "*.csv")],
-                                         initialdir="/")
+# window dimensions
+width = 400
+height = 300
 
-df_list = []
-for file_path in file_paths:
-    df = pd.read_csv(file_path)
-    df_list.append(df)
+# screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-merged_df = pd.concat(df_list, axis=1)
+# coordinates
+x = (screen_width - width) // 2
+y = (screen_height - height) // 2
 
-output_path = filedialog.asksaveasfilename(title="Save Merged CSV File As",
-                                           defaultextension=".csv",
-                                           filetypes=[("CSV Files", "*.csv")],
-                                           initialdir="/")
+# setting the position of the window
+root.geometry(f"{width}x{height}+{x}+{y}")
+file_paths = ""
 
-merged_df.to_csv(output_path, index=False)
 
-print(f"Merged CSV file saved at {output_path}")
+def choose():
+    global file_paths
+    file_paths = filedialog.askopenfilenames(title="Select CSV files to merge",
+                                             filetypes=[("CSV Files", "*.csv")],
+                                             initialdir="/")
+    if file_paths != "":
+        button.config(text="save", command=save)
+    else:
+        root.quit()
+
+
+def save():
+    df_list = []
+    for file_path in file_paths:
+        df = pd.read_csv(file_path)
+        df_list.append(df)
+
+    merged_df = pd.concat(df_list, axis=1)
+
+    output_path = filedialog.asksaveasfilename(title="Save Merged CSV File As",
+                                               defaultextension=".csv",
+                                               filetypes=[("CSV Files", "*.csv")],
+                                               initialdir="/")
+
+    merged_df.to_csv(output_path, index=False)
+
+    button.config(text="choose", command=choose)
+
+    label.config(text=f"Merged CSV file saved at {output_path}")
+
+
+label = tk.Label(root, text="Merge CSV files")
+label.pack(pady=50)
+button = tk.Button(root, text="Select", command=choose)
+button.pack()
+
+root.mainloop()
